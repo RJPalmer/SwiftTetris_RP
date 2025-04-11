873198293a2da8e8f23ad072c604b799ad4c12b2
Robert Palmer <Awriter247@gmail.com>
2025-04-11 12:35:24 -0400

    added loading screen to the application.

---
+++ SwiftTetris_RP/SwiftTetris_RP/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  SwiftTetris_RP
+//
+//  Created by Robert Palmer on 4/10/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
