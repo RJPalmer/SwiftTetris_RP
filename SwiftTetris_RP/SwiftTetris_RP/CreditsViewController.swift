545ded6735a7a62aca824a2c041c9e01b2128a19
Robert Palmer <Awriter247@gmail.com>
2025-06-28 21:18:10 -0400

    Add UI to CreditsViewController with label and button
    Implemented viewDidLoad in CreditsViewController to display a credits label and a dismiss button. The button allows users to return to the main menu. Also added and updated some workspace and system files.

--- SwiftTetris_RP/SwiftTetris_RP/CreditsViewController.swift
+++ SwiftTetris_RP/SwiftTetris_RP/CreditsViewController.swift
@@ -15,4 +15,36 @@ class CreditsViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+        let creditLabel = UILabel()
+        creditLabel.text = "SwiftTetris_RP\nCreated by Robert Palmer"
+        creditLabel.numberOfLines = 0
+        creditLabel.textAlignment = .center
+        creditLabel.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        creditLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        let dismissButton = UIButton(type: .system)
+        dismissButton.setTitle("Back to Main Menu", for: .normal)
+        dismissButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        dismissButton.addTarget(self, action: #selector(dismissCredits), for: .touchUpInside)
+        dismissButton.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(creditLabel)
+        view.addSubview(dismissButton)
+
+        NSLayoutConstraint.activate([
+            creditLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            creditLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
+            dismissButton.topAnchor.constraint(equalTo: creditLabel.bottomAnchor, constant: 20),
+            dismissButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+    @objc private func dismissCredits() {
+        dismiss(animated: true, completion: nil)
+    }
 }
